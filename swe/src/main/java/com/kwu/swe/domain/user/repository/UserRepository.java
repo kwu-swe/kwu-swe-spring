0a3ad34ab891a5ca496df79097e29fbeac35eddd
hann <wjdgks1233@gmail.com>
2025-03-18 22:59:02 +0900

    setting user part class and comment out all of it

---
+++ swe/src/main/java/com/kwu/swe/domain/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+//package com.kwu.swe.domain.user.repository;
+//
+//import com.kwu.swe.domain.user.entity.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//}
---
+++ swe/src/main/java/com/kwu/swe/domain/user/service/UserQueryServiceImpl.java
@@ -0,0 +1,4 @@
+//package com.kwu.swe.domain.user.service;
+//
+//public class UserQueryServiceImpl {
+//}

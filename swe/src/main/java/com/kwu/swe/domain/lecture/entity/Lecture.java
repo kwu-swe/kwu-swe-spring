f8fc4221414f0705e5851f855f984f94c90cb9e2
hann <wjdgks1233@gmail.com>
2025-03-25 16:32:49 +0900

    doc : comment out #3

--- swe/src/main/java/com/kwu/swe/domain/lecture/entity/Lecture.java
+++ swe/src/main/java/com/kwu/swe/domain/lecture/entity/Lecture.java
@@ -19,14 +19,18 @@ public class Lecture {
 
     private int sizeLimit;
 
+    //강의 전, 강의 중, 강의 후
+    //학점 부여를 위한 status
     @Enumerated(EnumType.STRING)
     @Column(name = "lecture_status", nullable = false)
     private LectureStatus lectureStatus;
 
+    //4개의 학기밖에 없으므로 entity -> enum으로 변경
     @Enumerated
     @Column(name = "semester", nullable = false, length = 50)
     private Semester semester;
 
+    //해당 강의의 스케줄 및 장소를 여러개 정할 수 있음
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "lecture_schedule_id")
     private LectureSchedule lectureSchedule;

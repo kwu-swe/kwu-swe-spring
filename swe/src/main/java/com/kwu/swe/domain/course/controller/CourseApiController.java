b1a989e9c28f40f2e3a811989792106f1ecf2781
hann <wjdgks1233@gmail.com>
2025-04-01 18:56:28 +0900

    refactor : appl apiResponseDto in controller methods #12

--- swe/src/main/java/com/kwu/swe/domain/course/controller/CourseApiController.java
+++ swe/src/main/java/com/kwu/swe/domain/course/controller/CourseApiController.java
@@ -2,6 +2,7 @@ package com.kwu.swe.domain.course.controller;
 
 import com.kwu.swe.domain.course.dto.RegisterCourseRequestDto;
 import com.kwu.swe.domain.course.service.CourseCommandService;
+import com.kwu.swe.presentation.payload.dto.ApiResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,7 @@ public class CourseApiController {
     private final CourseCommandService courseCommandService;
 
     @PostMapping
-    public Long registerCourse(@RequestBody RegisterCourseRequestDto dto) {
-        return courseCommandService.registerCourse(dto);
+    public ApiResponseDto<Long> registerCourse(@RequestBody RegisterCourseRequestDto dto) {
+        return ApiResponseDto.onSuccess(courseCommandService.registerCourse(dto));
     }
 }

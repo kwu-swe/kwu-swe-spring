5638cebb2d624634c3abada3e712072c1f8a2af4
hann <wjdgks1233@gmail.com>
2025-04-08 15:37:40 +0900

    feat : create get all course api #24

--- swe/src/main/java/com/kwu/swe/domain/course/service/CourseQueryService.java
+++ swe/src/main/java/com/kwu/swe/domain/course/service/CourseQueryService.java
@@ -1,4 +1,10 @@
 package com.kwu.swe.domain.course.service;
 
+import com.kwu.swe.domain.course.entity.Course;
+
+import java.util.List;
+
 public interface CourseQueryService {
+
+    List<Course> getAllCourse();
 }

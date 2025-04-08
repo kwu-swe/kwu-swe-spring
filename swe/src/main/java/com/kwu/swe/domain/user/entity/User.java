b5be1c556086a3ac7ce1e80e49c50a8dcdf56bb4
hann <wjdgks1233@gmail.com>
2025-04-08 13:03:13 +0900

    feat : create user entity #18

--- swe/src/main/java/com/kwu/swe/domain/user/entity/User.java
+++ swe/src/main/java/com/kwu/swe/domain/user/entity/User.java
@@ -1,4 +1,27 @@
-//package com.kwu.swe.domain.user.entity;
-//
-//public class User {
-//}
+package com.kwu.swe.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String studentNumber;
+
+    private String phoneNumber;
+
+
+}

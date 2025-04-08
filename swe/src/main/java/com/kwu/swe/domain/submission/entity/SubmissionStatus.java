8d7bf26b62ead06c257a66f32d250fcf8b6f3ac1
hann <wjdgks1233@gmail.com>
2025-04-08 23:16:24 +0900

    feat : enumApiController #24

--- swe/src/main/java/com/kwu/swe/domain/submission/entity/SubmissionStatus.java
+++ swe/src/main/java/com/kwu/swe/domain/submission/entity/SubmissionStatus.java
@@ -1,6 +1,14 @@
 package com.kwu.swe.domain.submission.entity;
 
-public enum SubmissionStatus {
-    SUBMITTED, // 제출 완료
-    LATE // 지각 제출
+import com.kwu.swe.global.interfaces.KeyedEnum;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SubmissionStatus implements KeyedEnum {
+    SUBMITTED("제출 완료"), // 제출 완료
+    LATE("제출 지각"); // 지각 제출
+
+    private final String key;
 }

3a57a20e6ad0e05003c944a20ba0c0709acd0b7f
hann <wjdgks1233@gmail.com>
2025-04-08 13:27:36 +0900

    feat : setting methods in command service interface #20

--- swe/src/main/java/com/kwu/swe/domain/user/service/UserCommandService.java
+++ swe/src/main/java/com/kwu/swe/domain/user/service/UserCommandService.java
@@ -1,4 +1,12 @@
 package com.kwu.swe.domain.user.service;
 
 public interface UserCommandService {
+
+    Long registerUser(RegisterUserRequestDto dto);
+
+    Long updateUserInfo(EditUserInfoRequestDto dto);
+
+    Long registerCourse(String studentNumber, Long lectureId);
+
+    Long addLecture(String studentNumber, Long lectureId);
 }

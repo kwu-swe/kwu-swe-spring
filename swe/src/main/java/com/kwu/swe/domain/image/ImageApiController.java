ff862cc1908abf573789c39fca7da795a60557b0
hann <wjdgks1233@gmail.com>
2025-05-07 20:42:39 +0900

    hotfix : resolve error

--- swe/src/main/java/com/kwu/swe/domain/image/ImageApiController.java
+++ swe/src/main/java/com/kwu/swe/domain/image/ImageApiController.java
@@ -3,10 +3,8 @@ package com.kwu.swe.domain.image;
 import com.kwu.swe.global.util.DataBucketUtil;
 import com.kwu.swe.presentation.payload.dto.ApiResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -18,7 +16,7 @@ public class ImageApiController {
 
     private final DataBucketUtil dataBucketUtil;
 
-    @PatchMapping
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ApiResponseDto<List<String>> convertFileToURL(@RequestPart List<MultipartFile> multipartFiles) {
         List<String> result = multipartFiles.stream().map(multipartFile ->
                         dataBucketUtil.appendPrefix(dataBucketUtil.uploadImage(multipartFile)))

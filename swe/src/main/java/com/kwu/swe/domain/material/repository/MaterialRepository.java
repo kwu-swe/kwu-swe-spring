0044be08d959edd76d4700979b964ed6ecf74f49
hann <wjdgks1233@gmail.com>
2025-05-14 22:16:23 +0900

    feat : create MaterialQueryService methods #43

--- swe/src/main/java/com/kwu/swe/domain/material/repository/MaterialRepository.java
+++ swe/src/main/java/com/kwu/swe/domain/material/repository/MaterialRepository.java
@@ -3,5 +3,8 @@ package com.kwu.swe.domain.material.repository;
 import com.kwu.swe.domain.material.entity.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MaterialRepository extends JpaRepository<Material, Long> {
+    List<Material> findByLectureId(Long lectureId);
 }
--- swe/src/main/java/com/kwu/swe/domain/material/service/MaterialQueryService.java
+++ swe/src/main/java/com/kwu/swe/domain/material/service/MaterialQueryService.java
@@ -1,4 +1,13 @@
 package com.kwu.swe.domain.material.service;
 
+
+import com.kwu.swe.domain.material.entity.Material;
+
+import java.util.List;
+
 public interface MaterialQueryService {
+
+    List<Material> getMaterialByLectureId(Long lectureId);
+
+    Material getSpecificMaterial(Long materialId);
 }

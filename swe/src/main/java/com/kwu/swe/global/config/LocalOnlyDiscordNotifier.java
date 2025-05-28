a97fd93c217704c79c42de9c3863933d8e5fa6cd
hann <wjdgks1233@gmail.com>
2025-05-28 22:15:56 +0900

    hotfix : lectureId -> lecture

--- swe/src/main/java/com/kwu/swe/global/config/LocalOnlyDiscordNotifier.java
+++ swe/src/main/java/com/kwu/swe/global/config/LocalOnlyDiscordNotifier.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
-@Profile({"prod", "han"})
+@Profile({"prod"})
 @RequiredArgsConstructor
 public class LocalOnlyDiscordNotifier {
 

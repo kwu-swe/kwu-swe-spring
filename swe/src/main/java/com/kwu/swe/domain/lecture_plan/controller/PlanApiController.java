651eb555e3fd8c2f4e674b8c11c2373204438df9
진세현 <devjinsehyun@gmail.com>
2025-05-30 00:39:27 +0900

    plan post 요청 기능

--- swe/src/main/java/com/kwu/swe/domain/lecture_plan/controller/PlanApiController.java
+++ swe/src/main/java/com/kwu/swe/domain/lecture_plan/controller/PlanApiController.java
@@ -1,17 +1,19 @@
 package com.kwu.swe.domain.lecture_plan.controller;
 
+import com.kwu.swe.domain.announcement.dto.AnnouncementSummaryDto;
+import com.kwu.swe.domain.announcement.entity.Announcement;
 import com.kwu.swe.domain.lecture_location.dto.RegisterLocationRequestDto;
 import com.kwu.swe.domain.lecture_plan.dto.RegisterPlanRequestDto;
 import com.kwu.swe.domain.lecture_plan.service.LecturePlanCommandService;
+import com.kwu.swe.domain.lecture_plan.service.LecturePlanCommandServiceImpl;
 import com.kwu.swe.presentation.payload.dto.ApiResponseDto;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/plans")
@@ -20,9 +22,23 @@ public class PlanApiController {
 
     private final LecturePlanCommandService lecturePlanCommandService;
 
-    @PostMapping
-    public ApiResponseDto<Long> registerPlan(@RequestBody RegisterPlanRequestDto dto,
+    @PostMapping("/lectures/{lectureId}")
+    public ApiResponseDto<Long> registerPlan(@PathVariable Long lectureId,
+                                             @RequestBody RegisterPlanRequestDto dto,
                                              @Parameter(hidden = true) @AuthenticationPrincipal UserDetails userDetails) {
-        return ApiResponseDto.onSuccess(lecturePlanCommandService.registerLocation(dto, userDetails.getUsername()));
+        return ApiResponseDto.onSuccess(lecturePlanCommandService.registerPlan(dto, userDetails.getUsername(), lectureId));
+    }
+
+    @GetMapping("/lectures/{lectureId}")
+    public ApiResponseDto<List<AnnouncementSummaryDto>> getAnnouncementsByLecture(@PathVariable Long lectureId) {
+        List<Announcement> announcements = announcementQueryService.getAnnouncementByLectureId(lectureId);
+        List<AnnouncementSummaryDto> result = announcements.stream().map(announcement ->
+                AnnouncementSummaryDto.builder()
+                        .announcementId(announcement.getId())
+                        .title(announcement.getTitle())
+                        .createdAt(announcement.getCreatedAt())
+                        .build()
+        ).toList();
+        return ApiResponseDto.onSuccess(result);
     }
 }
--- swe/src/main/java/com/kwu/swe/domain/lecture_plan/service/LecturePlanCommandService.java
+++ swe/src/main/java/com/kwu/swe/domain/lecture_plan/service/LecturePlanCommandService.java
@@ -3,5 +3,5 @@ package com.kwu.swe.domain.lecture_plan.service;
 import com.kwu.swe.domain.lecture_plan.dto.RegisterPlanRequestDto;
 
 public interface LecturePlanCommandService {
-    Long registerLocation(RegisterPlanRequestDto dto, String code);
+    Long registerPlan(RegisterPlanRequestDto dto, String code, Long lectureId);
 }

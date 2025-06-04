96d568e631e987579e75aefc24ad88d15a8853d9
hann <wjdgks1233@gmail.com>
2025-06-04 23:26:17 +0900

    hotfix : add cors

--- swe/src/main/java/com/kwu/swe/global/config/WebMvcConfig.java
+++ swe/src/main/java/com/kwu/swe/global/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/api/**") //CORS 적용할 URL 패턴
-                .allowedOriginPatterns("http://localhost:5173","https://local.tosel.co.kr") //자원 공유 오리진 지정
+                .allowedOriginPatterns("http://localhost:5173","https://local.tosel.co.kr","https://klasplus.netlify.app") //자원 공유 오리진 지정
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") //요청 허용 메서드
                 .allowedHeaders("*") //요청 허용 헤더
                 .allowCredentials(true) //요청 허용 쿠키
